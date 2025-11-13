b591061b42ef5496c467dd6b99228250aa6bb31a
youknowmenig69 <youknowmexd6969@gmail.com>
2025-11-13 09:02:02 +0530

    Update customize.sh

--- unica/patches/AI/customize.sh
+++ unica/patches/AI/customize.sh
@@ -6,10 +6,4 @@ ADD_TO_WORK_DIR "pa3qxxx" "system" "system/priv-app/PhotoEditor_AIFull" 0 0 755 
 ADD_TO_WORK_DIR "pa3qxxx" "system" "system/app/SketchBook" 0 0 755 "u:object_r:system_file:s0"
 
 
-echo "Spoofing DeviceImage.png"
-[ -f "$WORK_DIR/system/system/etc/init/spoof.rc" ] && rm -f "$WORK_DIR/system/system/etc/init/spoof.rc"
-{
-    echo "on boot"
-    echo "    setprop ril.product_code SM-S937BZSBINS"
-} >> "$WORK_DIR/system/system/etc/init/spoof.rc"
-SET_METADATA "system" "system/etc/init/spoof.rc" 0 0 644 "u:object_r:system_file:s0"
+
