8d7579bf348c7155f92cea9d6147d864c9b0a389
youknowmenig69 <youknowmexd6969@gmail.com>
2025-11-17 20:01:30 +0530

    Update customize.sh

--- unica/patches/AI/customize.sh
+++ unica/patches/AI/customize.sh
@@ -5,6 +5,6 @@ ADD_TO_WORK_DIR "pa3qxxx" "system" "system/priv-app/Moments" 0 0 755 "u:object_r
 ADD_TO_WORK_DIR "pa3qxxx" "system" "system/priv-app/SamsungSmartSuggestions" 0 0 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa3qxxx" "system" "system/priv-app/PhotoEditor_AIFull" 0 0 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa3qxxx" "system" "system/app/SketchBook" 0 0 755 "u:object_r:system_file:s0"
-
+SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_PERSONALIZED_DATA_CORE" "TRUE"
 
 
